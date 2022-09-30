33bc4c1c6bbe8dc6f4729fcfea2b835420442848
genevievemacabio <89491858+genevievemacabio@users.noreply.github.com>
2022-09-30 18:42:28 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title> Midterm Exercise 1 - WDD </title>
+</head>
+<body>
+<center>
+
+<?php
+	$name = "Genevieve A. Macabio";
+	$college_program = "Computer Engineering";
+	$year = "3rd Year";
+	$section = "Web Design and Development - 58043";
+	$string = "Today, I will learn PHP programming";
+	
+	echo "$name";
+	echo "<br>";
+	echo "$college_program";
+	echo "<br>";
+	echo "$year";
+	echo "<br>";
+	echo "$section";
+	echo "<br>";
+	echo $string;
+	echo "<br>"
+	
+?>
+
+</body>
+</html>
